10d1afd8778bb6d99ee2eb246e0126ab38c71be4
Stas <38349449+morozik11@users.noreply.github.com>
2021-10-28 00:01:54 +0300

    Create webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: 'index_bundle.js',
+  },
+  plugins: [new HtmlWebpackPlugin({
+    template: './src/index.html'
+  })],
+  module: {
+    rules: [
+      {
+        test: /\.js?$/,
+        use: [
+          {
+            loader: 'babel-loader'
+          },
+        ],
+        exclude: [/node_modules/],
+      },
+    ],
+  },
+}
