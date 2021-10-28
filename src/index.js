29159b843a8f82adfdacc5594253747056a9440e
Stas <38349449+morozik11@users.noreply.github.com>
2021-10-28 20:44:45 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import { fromEvent, of, switchMap, takeUntil, map, throwError, catchError, tap } from 'rxjs'
+import { fromEvent, of, switchMap, takeUntil, map, throwError, catchError, mergeMap, delay } from 'rxjs'
 
 const div = document.getElementById('dragAndDrop')
 const target = document.getElementById('target')
@@ -9,76 +9,76 @@ const mouseup$ = fromEvent(document, 'mouseup')
 
 let counter = 0
 
-const average = window.outerHeight/2 + window.outerHeight/4
+const average = window.innerHeight/2
 const targetPosition = target.getBoundingClientRect()
 
 const dragAndDrop = mousedown$.pipe(
   switchMap(startEvent => {
+    let pageY = null
     return mousemove$.pipe(
-      map(moveEvent => ({
-        type: 'mousemove',
-        targetEvent: moveEvent,
-        deltaX: moveEvent.pageX - startEvent.pageX,
-        deltaY: moveEvent.pageY - startEvent.pageY,
-        startOffsetX: startEvent.offsetX,
-        startOffsetY: startEvent.offsetY
-      })),
+      map(moveEvent => {
+          pageY = moveEvent.pageY
+          if (moveEvent.pageY >= average) {
+            document.dispatchEvent(new Event("mouseup"))
+          }
+          return {
+            type: 'mousemove',
+            targetEvent: moveEvent,
+            deltaX: moveEvent.pageX - startEvent.pageX,
+            deltaY: moveEvent.pageY - startEvent.pageY,
+            startOffsetX: startEvent.offsetX,
+            startOffsetY: startEvent.offsetY
+          }
+        }
+      ),
       takeUntil(mouseup$.pipe(
-        switchMap(ev => throwError (() => ev))
-      )),  
-      switchMap(ev => {
-        if (ev.targetEvent.pageY >= average) {
-          return of({type: 'add', targetEvent: ev}).pipe(
-            tap(() => {
-              target.style.backgroundColor = 'rgb(118, 201, 145)'
+        switchMap(ev => {
+          return throwError (() => ev)
+        })
+      )),
+      catchError(() => {
+        if (pageY >= average) {
+          return of({type: 'add'}, {type: 'hide'}, {type: 'back'}, {type: 'show'}).pipe(
+            mergeMap((item, index) => {
+              return of(item).pipe(
+                delay(index*1000)
+              )
             })
           )
         }
-        return of(ev)
-      }),
-      catchError(() => of([]).pipe(
-        switchMap(eve => of({type: 'back', targetEvent: eve}).pipe())
-      ))
+        
+        return of({type: 'back'})
+      })
     )
   })
 )
 
-const makeSubscription = (container) => {
-  container = dragAndDrop.subscribe(
-    ev => {
-      if (ev.type === 'mousemove') {
-        if (div.style.transition != `none`) div.style.transition = `none`
-        div.style.left = `${ev.targetEvent.x - ev.startOffsetX}px`
-        div.style.top = `${ev.targetEvent.y - ev.startOffsetY}px`
-      } else {
-        if (ev.type == 'back') {
-          div.style.transition = `left 1s, top 1s`
-          div.style.left = `0px`
-          div.style.top = `0px`
-        }
-        if (ev.type == 'add') {
-          container.unsubscribe()
-          target.innerText = `${counter = counter + 1}`
-          div.style.transition = `left 1s, top 1s`
-          div.style.left = `${targetPosition.x}px`
-          div.style.top = `${targetPosition.y}px`
-          setTimeout(() => {
-            div.style.display = 'none'
-            div.style.left = `0px`
-            div.style.top = `0px`
-            makeSubscription(container)  
-          },1000)
-          setTimeout(() => {
-            div.style.display = 'block'
-            target.style.backgroundColor = 'white'
-          },2000)
-        }
+let subscription = dragAndDrop.subscribe(
+  ev => {
+    if (ev.type === 'mousemove') {
+      if (div.style.transition != `none`) div.style.transition = `none`
+      div.style.left = `${ev.targetEvent.x - ev.startOffsetX}px`
+      div.style.top = `${ev.targetEvent.y - ev.startOffsetY}px`
+    } else {
+      if (ev.type == 'add') {
+        div.style.transition = `left 1s, top 1s`
+        div.style.left = `${targetPosition.x}px`
+        div.style.top = `${targetPosition.y}px`
+        target.innerText = `${counter = counter + 1}`
+      }
+      if (ev.type == 'hide') {
+        div.style.display = `none`
+      }
+      if (ev.type == 'back') {
+        div.style.transition = `left 1s, top 1s`
+        div.style.left = `0px`
+        div.style.top = `0px`
+      }
+      if (ev.type == 'show') {
+        div.style.display = `block`
       }
     }
-  )
-}
-
-let subscription = null
-makeSubscription(subscription)
+  }
+)
 
 
